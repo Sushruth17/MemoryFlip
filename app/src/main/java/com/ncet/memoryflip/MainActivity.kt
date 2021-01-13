cf760d9894a2508a2813f5fec7c1f1575a0c1e59
sushruth17 <sushruth.sushe@gmail.com>
2021-01-13 17:12:14 +0530

    initial commit

---
+++ app/src/main/java/com/ncet/memoryflip/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ncet.memoryflip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
